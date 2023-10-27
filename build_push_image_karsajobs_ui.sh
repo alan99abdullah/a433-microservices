37ac17458d1a072b6374fa99f8b6948c6971c5f2
alan99abdullah <abng.long492@gmail.com>
2023-10-27 16:09:02 +0700

    Adding the build_push_image_karsajobs_ui.sh file to build and push the image

---
+++ build_push_image_karsajobs_ui.sh
@@ -0,0 +1,11 @@
+
+#!/bin/bash
+
+# Build Docker image
+docker build -t ghcr.io/alan99abdullah/karsajobs-ui:latest .
+
+# Log in to GitHub Container Registry
+docker login ghcr.io -u alan99abdullah -p $GH_PACKAGES_TOKEN
+
+# Push Docker image to GitHub Container Registry
+docker push ghcr.io/alan99abdullah/karsajobs-ui:latest
